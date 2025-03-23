387f651aa3d59a63afb2a3ad01bc9e8250311544
AsutoshDalei <asutoshdalei@gmail.com>
2025-03-23 14:14:46 -0400

    hello world from go

---
+++ hellogo.go
@@ -0,0 +1,17 @@
+package main
+
+//https://www.youtube.com/watch?v=YzLrWHZa-Kc&ab_channel=DerekBanas
+/*
+Multiline
+Comment
+*/
+
+import (
+	"fmt"
+)
+
+var pl = fmt.Println
+
+func main() {
+	pl("Hello World")
+}
